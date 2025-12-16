e21598ba7e20f3c78d1913d92e28950bcd19609d
MaratonaDeJava <pierinak0@gmail.com>
2025-12-15 23:38:42 -0400

    Initial commit: first working version

---
+++ script.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#10B981',
+                secondary: '#059669',
+                dark: '#064E3B',
+                light: '#D1FAE5',
+            }
+        }
+    }
+}
